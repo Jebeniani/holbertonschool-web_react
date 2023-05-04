39a319e2b50dcf0c380850d0d7f498a0c6d63f93
jebeniani <aymenjebeniani@gmail.com>
2023-05-04 22:30:14 +0100

    dashboard main

---
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,27 @@
+import $ from 'jquery';
+import debounce from 'lodash/debounce';
+
+const button = $('<button>', {
+    text: 'Click here to get started'
+});
+
+const count = $('<p>', {
+    id: 'count'
+});
+
+$('body').append(
+    $('<p>').text('Holberton Dashboard'),
+    $('<p>').text('Dashboard data for the students'),
+    button,
+    count,
+    $('<p>').text('Copyright - Holberton School')
+);
+
+let counter = 0;
+
+function updateCounter() {
+    counter++;
+    count.text(`${counter} clicks on the button`);
+}
+
+button.on('click', debounce(updateCounter, 500));
