14d1fe12827f39fdc698861d67b539448d84c3f0
jebeniani <aymenjebeniani@gmail.com>
2023-05-04 22:40:04 +0100

    eslint fix

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -2,26 +2,27 @@ import $ from 'jquery';
 import debounce from 'lodash/debounce';
 
 const button = $('<button>', {
-    text: 'Click here to get started'
+  text: 'Click here to get started',
 });
 
 const count = $('<p>', {
-    id: 'count'
+  id: 'count',
 });
 
 $('body').append(
-    $('<p>').text('Holberton Dashboard'),
-    $('<p>').text('Dashboard data for the students'),
-    button,
-    count,
-    $('<p>').text('Copyright - Holberton School')
+  $('<p>').text('Holberton Dashboard'),
+  $('<p>').text('Dashboard data for the students'),
+  button,
+  count,
+  $('<p>').text('Copyright - Holberton School'),
 );
 
 let counter = 0;
 
 function updateCounter() {
-    counter++;
-    count.text(`${counter} clicks on the button`);
+  // eslint-disable-next-line no-plusplus
+  counter++;
+  count.text(`${counter} clicks on the button`);
 }
 
 button.on('click', debounce(updateCounter, 500));
