ac418d84e7278fac3a310de5159d094e8d0357c7
jebeniani <aymenjebeniani05@gmail.com>
2023-06-13 21:24:33 +0100

    Converting into an Immutable object using fromJS

---
+++ 0x07-React_Immutable/0-fromjs.js
@@ -0,0 +1,8 @@
+/*
+  *Task 0 - Converting into an Immutable object using fromJS
+*/
+import { fromJS } from 'immutable';
+
+export default function getImmutableObject(object) {
+    return fromJS(object);
+};
